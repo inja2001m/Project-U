560acfd784c646ac46c5f6a1827503464564a3b5
inja2001m <dopa_1@naver.com>
2017-09-17 23:34:26 +0900

    Create GuidedBullet.cs

---
+++ Project U/Assets/Resoruces/Scripts/Objects/Bullet/GuidedBullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuidedBullet : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject target;
+
+    public float m_Speed;
+
+    private int damage;
+    public int m_Damage
+    {
+        get
+        {
+            return this.damage;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        this.transform.Translate(Vector3.forward * m_Speed * Time.fixedDeltaTime);
+    }
+}
