000b292740ce1ba416953d0e9bfbe4a5cdb26a66
inja2001m <dopa_1@naver.com>
2017-09-17 23:34:50 +0900

    Update GuidedBulletDetector.cs

---
+++ Project U/Assets/Resoruces/Scripts/Objects/Bullet/GuidedBulletDetector.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuidedBulletDetector : MonoBehaviour
+{
+    public GameObject m_Target;
+
+    private bool m_IsFirst;
+
+    void Start()
+    {
+        m_IsFirst = true;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enermy")
+        {
+            m_Target = other.gameObject;
+            Destroy(this);
+        }
+    }
+}
