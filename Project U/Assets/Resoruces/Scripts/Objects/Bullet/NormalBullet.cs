fd651bd8940de42cfce6709b45164377c113d23b
inja2001m <dopa_1@naver.com>
2017-09-17 23:34:11 +0900

    Create NormalBullet.cs

---
+++ Project U/Assets/Resoruces/Scripts/Objects/Bullet/NormalBullet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NormalBullet : MonoBehaviour
+{
+    private int damage;
+    public int m_Damage
+    {
+        get
+        {
+            return this.damage;
+        }
+    }
+
+    public float m_Speed;
+
+    void FixedUpdate()
+    {
+        this.transform.Translate(Vector3.forward * m_Speed * Time.fixedDeltaTime);
+    }
+}
