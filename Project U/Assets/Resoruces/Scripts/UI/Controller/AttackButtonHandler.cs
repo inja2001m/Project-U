863344bc545fbd19afe552898defcf2ddabee265
inja2001m <dopa_1@naver.com>
2017-09-17 23:33:39 +0900

    Create AttackButtonHandler.cs

---
+++ Project U/Assets/Resoruces/Scripts/UI/Controller/AttackButtonHandler.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackButtonHandler : MonoBehaviour
+{
+    public enum AttackSkill_Type
+    {
+        NONE,
+        NORMAL,
+        SPECIAL
+    }
+
+    private AttackSkill_Type eAttackSkillType;
+    public AttackSkill_Type m_eAttackSkillType
+    {
+        get
+        {
+            return this.eAttackSkillType;
+        }
+    }
+
+    private bool m_IsOnlyOne;
+
+    void Start()
+    {
+        m_IsOnlyOne = false;
+        eAttackSkillType = AttackSkill_Type.NONE;
+    }
+
+    public void ClickedNormalAttack()
+    {
+        if (m_IsOnlyOne)
+            return;
+
+        eAttackSkillType = AttackSkill_Type.NORMAL;
+        m_IsOnlyOne = true;
+    }
+
+    public void ClickedSpecialAttack()
+    {
+        if (m_IsOnlyOne)
+            return;
+
+        eAttackSkillType = AttackSkill_Type.SPECIAL;
+        m_IsOnlyOne = true;
+    }
+
+    public void ClickedUpButton()
+    {
+        m_IsOnlyOne = false;
+        eAttackSkillType = AttackSkill_Type.NONE;
+    }
+}
