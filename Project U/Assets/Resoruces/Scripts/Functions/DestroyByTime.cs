ba38ada63fc17a98ec8397f88f775fa692a6fda3
inja2001m <dopa_1@naver.com>
2017-09-17 23:35:11 +0900

    Create DestroyByTime.cs

---
+++ Project U/Assets/Resoruces/Scripts/Functions/DestroyByTime.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByTime : MonoBehaviour
+{
+    [SerializeField]
+    public float m_LifeTime;
+
+    void Start()
+    {
+        Destroy(gameObject, m_LifeTime);
+    }
+}
